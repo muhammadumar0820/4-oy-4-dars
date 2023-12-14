3bd63a3a179c9155068ac57c62f5fa6ab7e7645c
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-14 16:59:40 +0300

    masallar bajarildi1

---
+++ index.js
@@ -0,0 +1,85 @@
+//Birinchi masala
+//let num = +prompt("Sonni kiriting...");
+//for (let i = 1; i <=num; i+1){
+//    if(num % 3 == 0 && num % 7 == 0){
+//        num=num+i;
+//    }
+//}
+//console.log(num);
+
+
+//Ikkinchi masala
+//let num;
+//for(let i = num; i<=1; i-1){
+//    sum+=i;
+//}
+//console.log(num);
+
+
+//Uchinchi masala
+//let n;
+//let res;
+//for (let i = 1; i <= n; i+1){
+//    if(n % 5 == 0 && n % 7 != 0){
+//        res=res+i;
+//    }
+//}
+//console.log(res);
+
+
+//To'rtinchi masala
+//let a;
+//let b; //(a<b)
+//let n;
+//let res;
+//for (let i = a; i <= b; i+1){
+//    if(n>a && n<0){
+//        res=res+i;
+//    }
+//}
+//console.log(res);
+
+
+//Beshinchi masala
+//let num = 3;
+//let res;
+//for(let i = 1; i <= num; i+1){
+//    res=res*i;
+//}
+//console.log(res);
+
+
+//Oltinchi masala
+//let num=+prompt("Sonni kiriting...");
+//for(let i = num; i <= 1; i-1){
+//    if(num%i==0){
+//        console.log(i);
+//    }
+//}
+
+
+//Yettinchi masala
+//let num=+prompt("Sonni kiriting...");
+//let=counter=0;
+//for(let i = num; i <= 1; i-1){
+//    if(num%i==0){
+//        counter=counter+1;
+//    }
+//}
+//console.log(counter);
+
+
+//Sakkizinchi masala
+//let num=+prompt("Sonni kiriting...");
+//let=counter=0;
+//let res; 
+//for(let i = num; i <= 1; i-1){
+//    if(num%i==0){
+//        counter=counter+1;
+//    }
+//}
+//if(counter==2){
+//    console.log("tub son");
+//}else{
+//    console.log("Tub emas");
+//}
